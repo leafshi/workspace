0f66bf2201a994eb930e8e18c161c2e0d73b821d
leaf_shi <leaf.shi@gmail.com>
2013-01-14 11:13:53 +0800

    报表目录定时扫描
    停止扫描

--- eos2/grails-app/jobs/org/leaf/eos2/birt/ReportScanJob.groovy
+++ eos2/grails-app/jobs/org/leaf/eos2/birt/ReportScanJob.groovy
@@ -2,8 +2,9 @@ package org.leaf.eos2.birt
 
 class ReportScanJob {
 
+	//停止扫描
     static triggers = {
-        cron name: 'reportScanJobTrigger', cronExpression: "0 5 * * * ?"
+        //cron name: 'reportScanJobTrigger', cronExpression: "0 5 * * * ?"
     }
 
     def group = "StartupGroup"
